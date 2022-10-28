afaecf6c45280ebf7382ef98b8aade51ad5d91d1
Pavel Tavoda <pavel.tavoda@gmail.com>
2022-10-28 17:45:23 +0200

    Multiple EntityManagers in flush interceptor

--- sculptor-framework/sculptor-framework-main/src/main/java/org/sculptor/framework/persistence/JpaFlushEagerInterceptor.java
+++ sculptor-framework/sculptor-framework-main/src/main/java/org/sculptor/framework/persistence/JpaFlushEagerInterceptor.java
@@ -1,13 +1,13 @@
 /*
- * Copyright 2013 The Sculptor Project Team, including the original 
+ * Copyright 2013 The Sculptor Project Team, including the original
  * author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -28,27 +28,29 @@ import javax.persistence.TransactionRequiredException;
  * <p>
  * We have to flush eagerly in order to detect OptimisticLockingException and do
  * proper rollback.
- * 
+ *
  * @author Patrik Nordwall
  */
-public class JpaFlushEagerInterceptor  {
+public class JpaFlushEagerInterceptor {
 
 	@PersistenceContext
-	private EntityManager entityManager;
+	private EntityManager[] entityManagers;
 
 	@AroundInvoke
-    public Object invoke(InvocationContext context) throws Exception {
+	public Object invoke(InvocationContext context) throws Exception {
 		Object result = context.proceed();
-        if (entityManager != null) {
-            try {
-                entityManager.flush();
-            } catch (EJBTransactionRolledbackException ignore) {
-                // already marked for rollback
-            } catch (TransactionRequiredException ignore) {
-                // already marked for rollback
-            }
-        }
-        return result;
-    }
+		if (entityManagers != null && entityManagers.length > 0) {
+			for (EntityManager e : entityManagers) {
+				try {
+					e.flush();
+				} catch (EJBTransactionRolledbackException ignore) {
+					// already marked for rollback
+				} catch (TransactionRequiredException ignore) {
+					// already marked for rollback
+				}
+			}
+		}
+		return result;
+	}
 
 }
